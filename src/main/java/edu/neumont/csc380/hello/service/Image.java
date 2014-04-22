82c5a66be107ee39e7cf4d5d08e53e4fd1fef74d
Dakotah <dreyes@student.neumont.edu>
2014-04-22 09:37:24 -0600

    filled image class

--- src/main/java/edu/neumont/csc380/hello/service/Image.java
+++ src/main/java/edu/neumont/csc380/hello/service/Image.java
@@ -1,5 +1,65 @@
 package edu.neumont.csc380.hello.service;
 
+@Entity
 public class Image {
-
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private String fileName;
+	private int width;
+	private int height;
+	private int fileSize;
+	
+	
+	public int getId()
+	{
+		return id;
+	}
+	
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	
+	public String getfileName()
+	{
+		return fileName;
+	}
+	
+	public void setfileName(String fileName)
+	{
+		this.fileName = fileName;
+	}
+	
+	public int getWidth()
+	{
+		return width;
+	}
+	
+	public void setWidth(int width)
+	{
+		this.width = width;
+	}
+	
+	public int getHeight()
+	{
+		return height;
+	}
+	
+	public void setHeight(int height)
+	{
+		this.height = height;
+	}
+	
+	public int getfileSize()
+	{
+		return fileSize;
+	}
+	
+	public void setfileSize(int fileSize)
+	{
+		this.fileSize = fileSize;
+	}
 }
