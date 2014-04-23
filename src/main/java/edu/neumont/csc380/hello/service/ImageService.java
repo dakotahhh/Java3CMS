38740fbb6ca18ae0d6761b71002e1dbf562db670
Dakotah <dreyes@student.neumont.edu>
2014-04-23 10:12:28 -0600

    image service class implemented properly. need to fill out consumes and produces

--- src/main/java/edu/neumont/csc380/hello/service/ImageService.java
+++ src/main/java/edu/neumont/csc380/hello/service/ImageService.java
@@ -1,8 +1,14 @@
 package edu.neumont.csc380.hello.service;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 
 @Path("/images")
@@ -10,4 +16,25 @@ import javax.ws.rs.Produces;
 @Produces("application/json")
 public interface ImageService {
 
+	@GET
+	@Path("/{id")
+	@Produces("image/jpg")
+	Response getImage(@PathParam("id") int id);
+	
+	@PUT
+	@Path("/{id}")
+	@Consumes("")
+	@Produces("")
+	Response updateImage(@PathParam("id") int id, Image image);
+	
+	@POST
+	@Consumes("")
+	@Produces("")
+	Response createImage(Image image);
+	
+	@DELETE
+	@Path("/{id}")
+	Response deleteImage(@PathParam("id") int id);
+	
+	
 }
