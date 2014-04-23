443b446b9527c215f60d8c1a5f623b197608613a
Dakotah <dreyes@student.neumont.edu>
2014-04-23 10:16:49 -0600

    videoservice implemented correctly need to fill out consumes and produces

--- src/main/java/edu/neumont/csc380/hello/service/VideoService.java
+++ src/main/java/edu/neumont/csc380/hello/service/VideoService.java
@@ -1,5 +1,39 @@
 package edu.neumont.csc380.hello.service;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+
+@Path("/video")
+@Consumes("application/json")
+@Produces("application/json")
 public interface VideoService {
 
+	@GET
+	@Path("/{id}")
+	@Produces("video/mov")
+	Response getVideo(@PathParam("id") int id);
+	
+	@PUT
+	@Path("/{id}")
+	@Consumes("")
+	@Produces("")
+	Response updateGreeting(@PathParam("id") int id, Video video);
+	
+	@POST
+	@Consumes("")
+	@Produces("")
+	Response createVideo(Video video);
+	
+	@DELETE
+	@Path("/{id}")
+	Response deleteGreeting(@PathParam("id") int id);
+	
 }
