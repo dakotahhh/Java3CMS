75f1ae08fa4c0181e84fc1da2285d7f6974e4487
Dakotah <dreyes@student.neumont.edu>
2014-04-24 10:38:37 -0600

    fixed exception mapper classes

--- src/main/java/edu/neumont/csc380/hello/service/ImageExceptionMapper.java
+++ src/main/java/edu/neumont/csc380/hello/service/ImageExceptionMapper.java
@@ -2,7 +2,12 @@ package edu.neumont.csc380.hello.service;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
 
+import org.springframework.stereotype.Service;
+
+@Provider
+@Service
 public class ImageExceptionMapper implements
 		ExceptionMapper<ImageNotFoundException> {
 
--- src/main/java/edu/neumont/csc380/hello/service/VideoExceptionMapper.java
+++ src/main/java/edu/neumont/csc380/hello/service/VideoExceptionMapper.java
@@ -2,7 +2,13 @@ package edu.neumont.csc380.hello.service;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
 
+import org.springframework.stereotype.Service;
+
+
+@Provider
+@Service
 public class VideoExceptionMapper implements
 		ExceptionMapper<VideoNotFoundException> {
 
