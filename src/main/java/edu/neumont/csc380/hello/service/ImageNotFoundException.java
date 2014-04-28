7d70ce97a34b5137eb8db861dfd911c5c4add5a7
Dakotah <dreyes@student.neumont.edu>
2014-04-28 09:59:46 -0600

    base for video and image not found exceptions

--- src/main/java/edu/neumont/csc380/hello/service/ImageNotFoundException.java
+++ src/main/java/edu/neumont/csc380/hello/service/ImageNotFoundException.java
@@ -2,6 +2,24 @@ package edu.neumont.csc380.hello.service;
 
 public class ImageNotFoundException extends Exception{
 
+	public ImageNotFoundException()
+	{
+		super();
+	}
 	
+	public ImageNotFoundException(String message)
+	{
+		super(message);
+	}
+	
+	public ImageNotFoundException(String message, Throwable cause)
+	{
+		super(message, cause);
+	}
+	
+	public ImageNotFoundException(Throwable cause)
+	{
+		super(cause);
+	}
 	
 }
